dd44f80ad8691aa7ed6db45eb542159b0118298f
Mateusz Pruss <235953@edu.p.lodz.pl>
2022-05-08 11:10:13 +0200

    zaladowanie przykladowych grafik

--- src/main/java/app/ageofspice/MapController.java
+++ src/main/java/app/ageofspice/MapController.java
@@ -7,6 +7,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
+
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Random;
